d588e54bd2b732e06c20b89fdfdd6b2298c3d3d7
m-zaremba <zaremba_michal@wp.pl>
2021-11-24 14:33:54 +0100

    volume control functionality added

--- script.js
+++ script.js
@@ -46,9 +46,52 @@ window.addEventListener("DOMContentLoaded", () => {
   // Use progress bar to navigate video
   const setProgress = (event) => {
     const newTime = event.offsetX / progressRange.offsetWidth;
-    progressBar.style.width = `${newTime * 100}%`
+    progressBar.style.width = `${newTime * 100}%`;
     video.currentTime = newTime * video.duration;
-  }
+  };
+
+  let lastVolume = 1;
+
+  const changeVolume = (event) => {
+    let volume = event.offsetX / volumeRange.offsetWidth;
+    if (volume < 0.1) {
+      volume = 0;
+    }
+    if (volume > 0.9) {
+      volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+
+    volumeIcon.className = "";
+
+    if (volume > 0.5) {
+      volumeIcon.classList.add("fas", "fa-volume-up");
+    } else if (volume < 0.5 && volume > 0) {
+      volumeIcon.classList.add("fas", "fa-volume-down");
+    } else if (volume === 0) {
+      volumeIcon.classList.add("fas", "fa-volume-off");
+    }
+    lastVolume = volume;
+  };
+
+  const toggleMute = () => {
+    volumeIcon.className = "";
+    if (video.volume) {
+      lastVolume = video.volume;
+      video.volume = 0;
+      volumeBar.style.width = 0;
+      volumeIcon.classList.add("fas", "fa-volume-mute");
+      volumeIcon.setAttribute("title", "Unmute");
+    } else {
+      video.volume = lastVolume;
+      volumeBar.style.width = `${lastVolume * 100}%`;
+      lastVolume > 0.5
+        ? volumeIcon.classList.add("fas", "fa-volume-up")
+        : volumeIcon.classList.add("fas", "fa-volume-down");
+      volumeIcon.setAttribute("title", "Mute");
+    }
+  };
 
   playBtn.addEventListener("click", togglePlay);
   video.addEventListener("click", togglePlay);
@@ -56,4 +99,6 @@ window.addEventListener("DOMContentLoaded", () => {
   video.addEventListener("timeupdate", updateProgress);
   video.addEventListener("canplay", updateProgress);
   progressRange.addEventListener("click", setProgress);
+  volumeRange.addEventListener("click", changeVolume);
+  volumeIcon.addEventListener("click", toggleMute);
 });
